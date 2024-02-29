91e9512c0b69147a4155b43e22686668a7fe4396
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-02-29 20:44:41 +0900

    debug: ignore google, naver platform source code

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -100,11 +100,51 @@ class BaseSocialLoginView(APIView):
 #         return self.user.objects.get_or_create(email=email)
 
 
-class KakaoLogin(BaseSocialLoginView):
+class KakaoLogin:  # (BaseSocialLoginView)
     platform = "kakao"
     token_url = SOCIAL.get("KAKAO_TOKEN_API")
     client_id = SOCIAL.get("KAKAO_CLIENT_ID")
     callback_uri = SOCIAL.get("KAKAO_CALLBACK_URI")
+
+    @property
+    def auth(self):
+        auth_fields = ["client_id", "client_secret", "callback_uri", "state"]
+        return {
+            "platform": self.platform,
+            "auth": {auth_field:  getattr(self, auth_field) for auth_field in auth_fields if getattr(self, auth_field)}
+        }
+
+    @csrf_exempt  # note : if postman testing needs csrftoken
+    def get(self, request: Request):
+        logger.info("Social login authorization")
+        access_token: str = services.request_access_token_by_auth_code(request, self.auth)
+        logger.debug(f"acces_token: {access_token}")
+        user_profile_request: Request = self.request_user_profile(access_token)
+        logger.debug(f"request user profile: {user_profile_request}")
+        user_profile_response: Dict[str, Any] = services.access_token_is_valid(user_profile_request)
+        logger.debug(f"user profile response: {user_profile_response}")
+        user_key, registration_params = self.get_account_user_primary_key(user_profile_response)
+        logger.debug(f"user key:{user_key}\nregister: {registration_params}")
+
+        try:
+            user: CustomUser = self.user.objects.get(email=user_key)
+            social_user: SocialAccount = SocialAccount.objects.get(user=user)
+            logger.debug(f"social user: {social_user}")
+
+            if social_user.provider != self.platform:
+                response_message = {"error": "no matching social type"}
+                return Response(response_message, status=status.HTTP_400_BAD_REQUEST)
+
+            if social_user:
+                return services.social_user_login(user)
+
+        except self.user.DoesNotExist:
+            user, created = self.simple_registration(user_key)
+            return services.user_does_not_exist(user, created, self.platform, registration_params)
+
+        except SocialAccount.DoesNotExist:
+            response_message = {"error": "소셜로그인 유저가 아닙니다."}
+            return Response(response_message, status=status.HTTP_400_BAD_REQUEST)
 
     def request_user_profile(self, access_token):
         headers = {"Authorization": f"Bearer {access_token}"}
